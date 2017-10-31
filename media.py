0b25e8438518fe53c26306e7ef69e5f786386605
Hwasu-Hyun <prpkr@naver.com>
2017-10-30 20:49:39 -0400

    fixed codes
    pep8 fixed codes

--- media.py
+++ media.py
@@ -1,58 +1,19 @@
 import webbrowser
-import fresh_tomatoes
-import media
 
-# Avengers movie: movie title, storyline, poster image and movie trailer.
-avengers = media.Movie(
-    "Avengers",
-    "American superhero film",
-    "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-    "https://www.youtube.com/watch?v=BSMEkYCk4Vo")
 
-# Taitanic movie: movie title, storyline, poster image and movie trailer.
-taitanic = media.Movie(
-    "Taitanic",
-    "American epic romance-disaster film",
-    "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
-    "https://www.youtube.com/watch?v=zCy5WQ9S4c0")
+class Movie():
+    """This docstring shoud explain what the Movie() class does"""
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-# IT movie: movie title, storyline, poster image and movie trailer.
-it = media.Movie(
-    "IT",
-    "Horro movie",
-    "https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg",
-    "https://www.youtube.com/watch?v=FnCdOQsX5kc")
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        """This docstring should explain the constructor method"""
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-# Saw movie: movie title, storyline, poster image and movie trailer.
-saw = media.Movie(
-    "Saw",
-    "Horro movie",
-    "https://upload.wikimedia.org/wikipedia/en/5/56/Saw_official_poster.jpg",
-    "https://www.youtube.com/watch?v=Lq2AzZB6DUE")
-
-# Jumanji movie: movie title, storyline, poster image and movie trailer.
-jumanji = media.Movie(
-    "Jumanji",
-    "American fantasy adventure film",
-    "https://upload.wikimedia.org/wikipedia/en/b/b6/Jumanji_poster.jpg",
-    "https://www.youtube.com/watch?v=8WaAUE4MXs8")
-
-# Grudge movie: movie title, storyline, poster image and movie trailer.
-grudge = media.Movie(
-    "Grudge",
-    "Horro movie",
-    "https://upload.wikimedia.org/wikipedia/en/9/91/The_Grudge_movie.jpg",
-    "https://www.youtube.com/watch?v=9ZCIjKFCxdw")
-
-# Hunger games movie: movie title, storyline, poster image and movie trailer.
-hunger_games = media.Movie(
-    "Hunger Games",
-    "A really real reality show",
-    "https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg",  # noqa
-    "https://www.youtube.com/watch?v=4S9a5V9ODuY")
-
-# Set the movies that will passed to the media file
-movies = [avengers, taitanic, it, saw, jumanji, grudge, hunger_games]
-
-# Open the HTML file in a webbrower via the fresh_tomatoes.py file
-fresh_tomatoes.open_movies_page(movies)
+    def show_trailer(self):
+        """This docstring should explain
+        what the show_trailer() function does"""
+        webbrowser.open(self.trailer_youtube_url)
